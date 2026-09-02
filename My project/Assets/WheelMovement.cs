badfc311693c072718ee1913b186800e65359bb8
daucholchan-debug <daucholchan@gmail.com>
2026-09-02 13:39:01 -0400

    Assignment01_DauChan

---
+++ My project/Assets/WheelMovement.cs
@@ -0,0 +1,23 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WheelMovement : MonoBehaviour
+{ 
+    [SerializeField] float moveSpeed = 5f; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+      
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 Move = new Vector3(x, y, 0f);
+        transform.Translate(Move * moveSpeed * Time.deltaTime);
+    }
+}
